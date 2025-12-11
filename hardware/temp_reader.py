2b220c8da0977baec9698b394aafc4a3db9a2d9a
somettt <somethinaws@gmail.com>
2025-12-11 16:33:09 +0900

    fix error

--- hardware/temp_reader.py
+++ hardware/temp_reader.py
@@ -1,8 +1,7 @@
-import Adafruit_DHT
+import adafruit_DHT
 import time
 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4   # GPIO번호
+DHT_SENSOR = adafruit_DHT.DHT11(board.D4)
 
 def read_temp():
     _, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
